579eec5e622b5e0c0764141be9707809002e2738
ldm8760 <ldm8760@rit.edu>
2025-11-13 15:25:42 -0500

    progress made in stats (G(X) per game) complete

--- README.md
+++ README.md
@@ -3,3 +3,38 @@ NBA Stats tracker / assistant
 What makes a current team good:
 
 Net rating = Offensive rating - defensive rating
+
+NBA statistical analysis
+
+Game utility stat:
+
+PTC + REB + AST + STL
+
+Where PTC = |PTS|
+PTC = (FGM - FG3M) x 2 + FG3M x 3 + FTM
+
+Giving us the Game utility function
+
+G(X)
+
+Where X = {Games}
+
+This has to be adjusted, does not consider each game
+
+G(X) can be compared to:
+Blocks
+Points?
+Turnovers
+Minutes
+
+Basic stats I probably need:
+0 PlAYER_ID
+3 GP
+4 GS
+5 MIN
+17 REB
+18 AST
+19 STL
+20 BLK
+21 TOV
+23 PTS
--- main.py
+++ main.py
@@ -1,10 +1,49 @@
 from nba_api.stats.endpoints import playercareerstats
+from nba_api.stats.endpoints import boxscoretraditionalv2
+from nba_api.stats.endpoints import leaguegamefinder
+import numpy as np
+
 import pandas as pd
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
 
 career = playercareerstats.PlayerCareerStats(player_id='203999') 
 
 # pandas data frames (optional: pip install pandas)
 df = pd.DataFrame(career.get_data_frames()[0])
+ud = df[["PLAYER_ID", "TEAM_ID", "GP", "GS", "FGM", "FG3M", "FTM", "MIN", "REB", "AST", "STL", "BLK", "TOV", "PTS"]]
+# ud means usefulData
 
-# json
-print(df)
+# ud["PTC"] = ud["FGM"] + ud["FTM"]
+# ud = ud.copy()
+# ud = ud["FGM"] + ud["FTM"]
+# print(ud)
+
+
+# gx = (ud["PTC"] + ud["REB"] + ud["AST"] + ud["STL"]) / ud["MIN"]
+
+# game_id = "0022400001"  # Example: 2024–25 regular season opener
+
+# boxscore = boxscoretraditionalv2.NBAStatsHTTP()
+# df = boxscore.get_data_frames()[0]
+# print(df.head())
+
+
+games = leaguegamefinder.LeagueGameFinder(player_or_team_abbreviation='P', player_id_nullable=203999)  # Jokic
+df = games.get_data_frames()[0]
+indGames = df[["PLAYER_ID", 'GAME_DATE', 'MATCHUP', "TEAM_ID", "FGM", "FG3M", "FTM", "MIN", "REB", "AST", "STL", "BLK", "TOV", "PTS"]]
+print(indGames)
+
+newDF = indGames
+newDF = newDF.copy()
+newDF["PTC"] = newDF["FGM"] + newDF["FTM"]
+newDF["G(X)"] = newDF["PTC"] + newDF["REB"] + newDF["AST"] + newDF["STL"]
+newDF["G(X)/MIN"] = round(newDF["G(X)"] / newDF["MIN"], 2)
+newDF["AST:TOV"] = np.where(
+    newDF["TOV"] == 0,       # if turnovers are zero
+    np.nan,               # or you could use np.inf, 0, etc.
+    round(newDF["AST"] / newDF["TOV"], 2)
+)
+print(newDF)
+# newDF["PTC"]
+
